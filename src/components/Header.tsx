96bd58304feb3b4aca20f5d7703bcd9016d843b5
Vedantjn <vedant.jain.indore@gmail.com>
2024-09-27 18:33:08 +0530

    added responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,11 @@ import { Button } from "./ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { BiLoaderCircle } from "react-icons/bi";
+import { Menu } from "lucide-react";
 
 export default function Header() {
   const [initialLoading, setInitialLoading] = useState<boolean>(true);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const { data: session, status } = useSession();
 
   useEffect(() => {
@@ -17,20 +19,44 @@ export default function Header() {
     }
   }, [status, session]);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="fixed top-0 w-full h-[60px] bg-black border-b border-white/60 p-3 flex justify-between items-center z-50">
-      <Link href="/">
-        <h2 className="font-bold text-xl">StableMax</h2>
-      </Link>
-      {initialLoading && status === "loading" ? (
-        <BiLoaderCircle className="animate-spin" />
-      ) : !session ? (
-        <div className="__menu">
-          <Button onClick={() => signIn("google")}>Login</Button>
+    <div className="fixed top-0 w-full bg-black border-b border-white/60 p-3 z-50">
+      <div className="flex justify-between items-center">
+        <Link href="/">
+          <h2 className="font-bold text-xl sm:text-2xl">StableMax</h2>
+        </Link>
+        <div className="hidden sm:flex">
+          {renderAuthButtons()}
         </div>
-      ) : (
-        <div className="flex gap-3 justify-center items-center">
-          <Button onClick={() => signOut()} variant="destructive">
+        <button className="sm:hidden" onClick={toggleMenu}>
+          <Menu size={24} />
+        </button>
+      </div>
+      {isMenuOpen && (
+        <div className="mt-3 sm:hidden">
+          {renderAuthButtons()}
+        </div>
+      )}
+    </div>
+  );
+
+  function renderAuthButtons() {
+    if (initialLoading && status === "loading") {
+      return <BiLoaderCircle className="animate-spin" />;
+    } else if (!session) {
+      return (
+        <div className="__menu">
+          <Button onClick={() => signIn("google")} className="w-full sm:w-auto">Login</Button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-col sm:flex-row gap-3 items-center">
+          <Button onClick={() => signOut()} variant="destructive" className="w-full sm:w-auto">
             Logout
           </Button>
           <Link href="/profile">
@@ -40,7 +66,7 @@ export default function Header() {
             </Avatar>
           </Link>
         </div>
-      )}
-    </div>
-  );
+      );
+    }
+  }
 }
